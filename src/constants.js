2ff87a4720b0d9a3623e5cdbe09c4d117735b282
HX001 <hongxu2015@gmail.com>
2020-11-18 17:42:21 -0500

    Update URL

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,6 @@
 export const TOKEN_KEY = 'TOKEN_KEY';
 
 //backend server
-export const URL = "https://fathomless-brook-68028.herokuapp.com/"
+export const URL = "https://backend-ece444.herokuapp.com/"
 export const ID = 'ID';
 export const PASSWORD = 'PASSWORD';
